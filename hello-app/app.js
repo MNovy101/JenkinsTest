228484ff4a1f2471b96b69497acad9227017880c
MNovy101 <56462194+MNovy101@users.noreply.github.com>
2025-07-22 16:03:02 -0700

    adding node

---
+++ hello-app/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('pew pew'));
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
