4097b8bffc76a7579eebda8df9000594b7e3cf59
MNovy101 <56462194+MNovy101@users.noreply.github.com>
2025-07-22 11:10:20 -0700

    fixing text

--- Hello.py
+++ Hello.py
@@ -3,7 +3,7 @@ def greet():
     return ("Hello, World!")
 
 def testWork():
-    return ("This should also work)")
+    return ("This should also work")
 
 if __name__ == "__main__":
     print(greet())
