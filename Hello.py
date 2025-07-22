8040afe0d07786f87b1d950baf173bc20e7e6248
MNovy101 <56462194+MNovy101@users.noreply.github.com>
2025-07-22 11:09:28 -0700

    testing tests

--- Hello.py
+++ Hello.py
@@ -2,6 +2,9 @@
 def greet():
     return ("Hello, World!")
 
+def testWork():
+    return ("This should also work)")
+
 if __name__ == "__main__":
     print(greet())
-    print("This should also work)")
+    print(testWork())
--- test_hello.py
+++ test_hello.py
@@ -2,3 +2,6 @@ import Hello
 
 def test_greeting():
     assert Hello.greet() == 'Hello, World!'
+
+def test_working():
+    assert Hello.testWork() == 'This should also work'
